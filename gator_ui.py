c836133e33563f370dbde666ff10ff9efc2eae87
sgt-tan <stanleygtan@gmail.com>
2017-11-30 16:01:58 -0500

    Update gator_ui.py

--- gator_ui.py
+++ gator_ui.py
@@ -128,9 +128,6 @@ class Ui_Form(object):
         self.speechApp()
     def speechApp(self):
         try:
-            engine.say("Hello, this is GatorWatch!")
-            engine.say("I can help you search for local movies, tv listings, or make a calender event.")
-            engine.runAndWait()
             print("Say something!")
             self.msgLayout.addWidget(MyWidget("Say something!\n"))
             
@@ -239,6 +236,9 @@ if __name__ == '__main__':
     print("A moment of silence, please...")
     with m as source: r.adjust_for_ambient_noise(source)
     print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    engine.say("Hello, this is GatorWatch!")
+    engine.say("I can help you search for local movies, tv listings, or make a calender event.")
+    engine.runAndWait()
     ex = App()
     ex.show()
     sys.exit(app.exec_())
